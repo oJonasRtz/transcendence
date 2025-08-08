0461c5133979c8cd55a413a8a07318f26819d1e9
Jonas Alberto Pereira <jopereir@.42sp.org.br>
2025-08-08 19:27:24 -0300

    prettier

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,11 @@
-import './index.css';
+import "./index.css";
 
 function App() {
-  return (
-      <div className="text-2xl flex  flex-wrap items-center justify-center w-screen h-screen">
-		<h1>
-			Wellcome to trancendence front-end
-		</h1>
-	  </div>
-  )
+	return (
+		<div className="text-2xl flex  flex-wrap items-center justify-center w-screen h-screen">
+			<h1>Wellcome to trancendence front-end</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
