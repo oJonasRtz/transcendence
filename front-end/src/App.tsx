5abaf5eccac4428f9501f556bcb8c262f871748c
Jonas Alberto Pereira <jopereir@.42sp.org.br>
2025-08-08 18:14:34 -0300

    ok

---
+++ front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+      <div className="text-2xl flex flex-wrap">
+		<h1>
+			Wellcome to trancendence front-end
+		</h1>
+	  </div>
+  )
+}
+
+export default App
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Importante para React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
