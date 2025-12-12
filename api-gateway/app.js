c726633f2ca90e3bbb54a2e7779470157e7c20d5
Fernando Ruan <fernandosingaporefuture@gmail.com>
2025-12-12 20:57:16 -0300

    liguei o errorHandler no app.js do api-gateway. Desativem caso desconfiarem de algum error principalmente em frontend

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -61,7 +61,7 @@ app.get("/boom", (req, reply) => {
 	throw new Error("Big Error");
 });
 
-//errorHandler(app);
+errorHandler(app);
 notFoundHandler(app);
 
 const isProduction = process.env.NODE_ENV === 'production';
