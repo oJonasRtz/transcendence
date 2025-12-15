bca4c1f90e9253eeec9c1f7395217eccc63a6633
jos-felipe <139470730+jos-felipe@users.noreply.github.com>
2025-12-15 16:07:30 -0300

    checkpoint: frontend/dashboard - add summarized cards

--- backend/db.js
+++ backend/db.js
@@ -14,20 +14,20 @@ async function dbPlugin(fastify, options) {
       password TEXT NOT NULL
       )`);
 
-    const insertUser = db.prepare(`
-      INSERT OR IGNORE INTO users (id, name, email, password)
-      VALUES (?, ?, ?, ?)
-    `)
+    // const insertUser = db.prepare(`
+    //   INSERT OR IGNORE INTO users (id, name, email, password)
+    //   VALUES (?, ?, ?, ?)
+    // `)
 
-    const insertedUsers = await Promise.all(
-      users.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        const userId = user.id || randomUUID();
-        return insertUser.run(userId, user.name, user.email, hashedPassword);
-      })
-    );
+    // const insertedUsers = await Promise.all(
+    //   users.map(async (user) => {
+    //     const hashedPassword = await bcrypt.hash(user.password, 10);
+    //     const userId = user.id || randomUUID();
+    //     return insertUser.run(userId, user.name, user.email, hashedPassword);
+    //   })
+    // );
 
-    console.log(`Inserted ${insertedUsers.length} users into the database.`);
+    // console.log(`Inserted ${insertedUsers.length} users into the database.`);
 
     db.exec(`CREATE TABLE IF NOT EXISTS invoices (
       id TEXT PRIMARY KEY,
@@ -37,22 +37,22 @@ async function dbPlugin(fastify, options) {
       date TEXT NOT NULL
     )`);
 
-    const insertInvoice = db.prepare(`
-      INSERT OR IGNORE INTO invoices (id, customer_id, amount, status, date) VALUES (?, ?, ?, ?, ?)
-    `);
+    // const insertInvoice = db.prepare(`
+    //   INSERT OR IGNORE INTO invoices (id, customer_id, amount, status, date) VALUES (?, ?, ?, ?, ?)
+    // `);
 
-    const insertedInvoices = invoices.map((invoice) => {
-      const invoiceId = randomUUID();
-      return insertInvoice.run(
-        invoiceId,
-        invoice.customer_id,
-        invoice.amount,
-        invoice.status,
-        invoice.date
-      );
-    })
+    // const insertedInvoices = invoices.map((invoice) => {
+    //   const invoiceId = randomUUID();
+    //   return insertInvoice.run(
+    //     invoiceId,
+    //     invoice.customer_id,
+    //     invoice.amount,
+    //     invoice.status,
+    //     invoice.date
+    //   );
+    // })
 
-    console.log(`Inserted ${insertedInvoices.length} invoices into the database.`);
+    // console.log(`Inserted ${insertedInvoices.length} invoices into the database.`);
 
     db.exec(`CREATE TABLE IF NOT EXISTS customers (
       id TEXT PRIMARY KEY,
@@ -61,35 +61,35 @@ async function dbPlugin(fastify, options) {
       image_url TEXT NOT NULL
       )`);
 
-    const insertCustomer = db.prepare(`INSERT OR IGNORE INTO customers (id, name, email, image_url) VALUES (?, ?, ?, ?)`);
+    // const insertCustomer = db.prepare(`INSERT OR IGNORE INTO customers (id, name, email, image_url) VALUES (?, ?, ?, ?)`);
 
-    const insertedCustomers = customers.map((customer) => {
-      const customerId = customer.id || randomUUID();
-      return insertCustomer.run(
-        customerId,
-        customer.name,
-        customer.email,
-        customer.image_url
-      );
-    })
+    // const insertedCustomers = customers.map((customer) => {
+    //   const customerId = customer.id || randomUUID();
+    //   return insertCustomer.run(
+    //     customerId,
+    //     customer.name,
+    //     customer.email,
+    //     customer.image_url
+    //   );
+    // })
 
-    console.log(`Inserted ${insertedCustomers.length} customers into the database.`);
+    // console.log(`Inserted ${insertedCustomers.length} customers into the database.`);
 
     db.exec(`CREATE TABLE IF NOT EXISTS revenue (
       month TEXT NOT NULL PRIMARY KEY,
       revenue INTEGER NOT NULL
     )`);
 
-    const insertRevenue = db.prepare(`INSERT OR IGNORE INTO revenue (month, revenue) VALUES (?, ?)`);
+    // const insertRevenue = db.prepare(`INSERT OR IGNORE INTO revenue (month, revenue) VALUES (?, ?)`);
 
-    const insertedRevenue = revenue.map((rev) => {
-      return insertRevenue.run(
-        rev.month,
-        rev.revenue
-      );
-    })
+    // const insertedRevenue = revenue.map((rev) => {
+    //   return insertRevenue.run(
+    //     rev.month,
+    //     rev.revenue
+    //   );
+    // })
 
-    console.log(`Inserted ${insertedRevenue.length} revenue records into the database.`);
+    // console.log(`Inserted ${insertedRevenue.length} revenue records into the database.`);
 
     fastify.decorate('db', db);
 
