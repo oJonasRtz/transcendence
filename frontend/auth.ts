917f425ff683fa4eafac7efc0ccaf23275907999
jos-felipe <139470730+jos-felipe@users.noreply.github.com>
2025-12-27 17:07:56 -0300

    mod: add the sign in functionality

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,6 +1,35 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { getUserByEmail } from 'app/lib/data'
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUserByEmail(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.passwordHash);
+          if (passwordsMatch) {
+            // NextAuth expects User.id to be a string, map numeric id to string
+            return {
+              ...user,
+              id: String(user.id),
+            };
+          }
+        }
+ 
+        return null;
+      },
+    }),
+  ],
 });
