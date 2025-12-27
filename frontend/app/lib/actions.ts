b27a48d587534be6580607a78c83962ff91de22c
jos-felipe <139470730+jos-felipe@users.noreply.github.com>
2025-12-27 17:10:10 -0300

    mod: update the login form

--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 export async function createMatch(formData: FormData) {
   const rawFormData = {
     userId: formData.get('userId'),
@@ -7,4 +10,23 @@ export async function createMatch(formData: FormData) {
     status: formData.get('status'),
   }
   console.log('Creating match with data:', rawFormData);
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
